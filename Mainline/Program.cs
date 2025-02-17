b6f853c895f865b1043e6be2d07704cc1681f977
Greg Doud <gpdoud@gmail.com>
2025-02-17 13:36:48 -0500

    Add project files.

---
+++ Mainline/Program.cs
@@ -0,0 +1,19 @@
+﻿//
+// add-package iText7
+//
+using iText.Kernel.Pdf; 
+using iText.Kernel.Pdf.Canvas.Parser;
+
+const string pdfPath = "C:\\repos\\Ken_read_pdf\\Mainline\\2-Git-Github-ADO-v1.pdf";
+
+GetPdfPageCount(pdfPath);
+
+static void GetPdfPageCount(string pdfPath)
+{
+    using (PdfReader reader = new PdfReader(pdfPath))
+    using (PdfDocument pdfDoc = new PdfDocument(reader))
+    {
+        int numberOfPages = pdfDoc.GetNumberOfPages();
+        Console.WriteLine($"The PDF has {numberOfPages} pages.");
+    }
+}
